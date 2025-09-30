4739a974e959d3be4ea2c76501752851b684642a
Matheus-quinzan <matheus.quinzan@stu.ocsb.ca>
2025-09-30 13:06:08 -0400

    Can show number of cookies

--- main.ts
+++ main.ts
@@ -4,5 +4,19 @@
  * Created on: Sep 2020
  * This program ...
 */
+let numberOfCookies: number = 0
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Shows Number Of Cookies
+
+input.onButtonPressed(Button.A, function () {
+    numberOfCookies = (numberOfCookies + 1)
+    basic.showString('' + (numberOfCookies).toString())
+})
+
+input.onButtonPressed(Button.B, function () {
+    numberOfCookies = (0)
+    basic.showString('' + (numberOfCookies).toString())
+})
